96954db0d8c448658728ab636d132e8e87621a9e
Delfina Munich <89984382+delfiam@users.noreply.github.com>
2022-09-16 12:06:47 -0300

    a ver si funciona

--- dist/app.module.js
+++ dist/app.module.js
@@ -9,6 +9,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const typeorm_1 = require("@nestjs/typeorm");
+const app_controller_1 = require("./app.controller");
+const app_service_1 = require("./app.service");
 const lugares_module_1 = require("./lugares/lugares.module");
 const usuarios_module_1 = require("./usuarios/usuarios.module");
 const usuario_entity_1 = require("./usuarios/usuario.entity");
@@ -18,20 +20,13 @@ const reviews_module_1 = require("./reviews/reviews.module");
 const auth_module_1 = require("./auth/auth.module");
 const seguido_module_1 = require("./seguido/seguido.module");
 const seguido_entity_1 = require("./seguido/seguido.entity");
-const lugares_controller_1 = require("./lugares/lugares.controller");
-const usuarios_service_1 = require("./usuarios/usuarios.service");
-const usuarios_controller_1 = require("./usuarios/usuarios.controller");
-const seguido_controller_1 = require("./seguido/seguido.controller");
-const seguido_service_1 = require("./seguido/seguido.service");
-const reviews_service_1 = require("./reviews/reviews.service");
-const reviews_controller_1 = require("./reviews/reviews.controller");
 const fs_1 = require("fs");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     (0, common_1.Module)({
-        providers: [usuarios_service_1.UsuariosService, seguido_service_1.SeguidoService, reviews_service_1.ReviewsService],
-        controllers: [lugares_controller_1.LugaresController, usuarios_controller_1.UsuariosController, seguido_controller_1.SeguidoController, reviews_controller_1.ReviewsController],
+        providers: [app_service_1.AppService],
+        controllers: [app_controller_1.AppController],
         imports: [
             lugares_module_1.LugarModule,
             usuarios_module_1.UsuariosModule,
